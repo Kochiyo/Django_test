885c393bbc6a1110aa77cf6a7e391d5202294cb7
julian5 <julian5421@outlook.com>
2018-08-07 10:48:06 +0800

    blog/models添加manager管理器

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,16 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (('draft','Draft'),('published','Published'),)
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts',on_delete=models.CASCADE)
@@ -19,3 +27,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
