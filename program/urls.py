8277e2ab9318e81fa668f9b170d090753a3c13d4
Kochiyo <2783153594@qq.com>
2018-08-08 16:53:58 +0800

    第一章结束，无异常

--- program/urls.py
+++ program/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
+    url(r'^blog/', include('blog.urls',namespace='blog')),
 ]
